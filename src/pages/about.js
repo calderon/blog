be96aa984d592868bc4e7b674c08951e1fd7b7ca
Daniel Calderon <dcalderon@avallain.com>
2020-03-05 22:18:25 +0100

    Update about.js with some information about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,27 @@ const About = props => {
   return (
     <div className="page--home">
       <Header />
-      <p>This is about me.</p>
+      <p>My name is Daniel Calderón.</p>
+      <p>
+        I'm a Front-end engineer, focused on web development, also an agile
+        enthusiast.
+      </p>
+      <p>
+        At the beginning, I worked with Rails to develop cross-platform web and
+        mobile applications.
+      </p>
+      <p>
+        While I worked at jobandtalent I had the opportunity to improve my
+        javascript skills.
+      </p>
+      <p>Currently, I'm training up in React and Gatsby as much as I can.</p>
+
+      <p>
+        Technologies: React, Gatsby, react-testing-library, styled-components,
+        CSS3 (SASS, Less), HTML5, RoR, RDW, UI, UX, Scrum, Git
+      </p>
+
+      <p>Knowledge is power, sharing it is pleasure.</p>
     </div>
   )
 }
