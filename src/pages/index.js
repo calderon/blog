943ba4583fbc74c8331d26a875b00edbea8b30e6
Daniel Calderon <dcalderon@avallain.com>
2020-03-05 22:28:52 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,13 @@ const Home = props => {
   return (
     <div className="page--home">
       <Header />
-      <div>Home page!</div>
+      <p>Hello! Welcome to my new coding lab, my own personal site.</p>
+      <p>Here you can see the progress of #100DaysOfGatsby challenges.</p>
+
+      <p>
+        You can find out the project on{" "}
+        <a href="https://github.com/calderon/blog">github</a>.
+      </p>
     </div>
   )
 }
