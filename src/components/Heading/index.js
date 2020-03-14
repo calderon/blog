f52c0c69286cf9b90895cbf60792ceec18e6e704
Daniel Calderon <daniel.calderon.portillo@gmail.com>
2020-03-14 18:27:37 +0100

    #3 Move Heading component to styled-components

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,15 +1,19 @@
 import React from "react"
+import styled, { css } from "styled-components"
+
+import { srOnly } from "../../assets/styles/mixins"
+
+const StyledHeading = styled.h1.attrs(props => ({
+  className: `heading heading--${props.as}`,
+}))`
+  color: ${props =>
+    props.inverted ? props.theme.palette.primary : props.theme.palette.primary};
+
+  ${props => (props.srOnly ? srOnly : "")};
+`
 
 const Heading = props => {
-  const tagName = props.as || "h1"
-
-  return React.createElement(
-    tagName,
-    {
-      className: props.className + ` heading heading--${tagName}`,
-    },
-    props.children
-  )
+  return <StyledHeading as="h1" {...props} />
 }
 
 export default Heading
