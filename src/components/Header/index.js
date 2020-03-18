609c1c6e877592002c2fefd9c98503371bf9c99c
Daniel Calderon <daniel.calderon.portillo@gmail.com>
2020-03-18 18:12:24 +0100

    #3 Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+import { devices } from "../../assets/styles/helpers"
+
 import Heading from "../Heading"
+import List from "../List"
+
+const StyledHeader = styled.header`
+  grid-column-start: 1;
+  grid-column-end: 4;
+  background-color: ${props => props.theme.colors.bright};
+  padding: 0 2.5rem;
+
+  @media ${devices.lg} {
+    padding: 0 5rem;
+  }
+`
 
 const HeaderTitle = styled(Heading)`
   font-size: 3.6rem;
@@ -18,15 +32,68 @@ const HeaderTitle = styled(Heading)`
   }
 `
 
+const HeaderNavigation = styled.nav``
+const HeaderMenu = styled(List)`
+  & > li {
+    margin-bottom: 1.25rem;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    @media ${devices.lg} {
+      display: inline;
+      margin-bottom: 0;
+    }
+  }
+`
+const HeaderMenuItem = styled.li`
+  font-size: 1.6rem;
+  text-transform: lowercase;
+
+  @media ${devices.lg} {
+    margin-right: 2.5rem;
+  }
+
+  .header__navigation__link {
+    color: ${props => props.theme.colors.link};
+  }
+`
+
 const Header = props => {
   return (
-    <header className="header">
+    <StyledHeader className="header">
       <HeaderTitle className="header__title">
         <Link className="header__title__link" to="/">
           Daniel Calderón
         </Link>
       </HeaderTitle>
-    </header>
+
+      <HeaderNavigation className="header__navigation">
+        <Heading className="header__navigation_title" srOnly>
+          Navigation menu
+        </Heading>
+
+        <HeaderMenu className="header__navigation__list" unstyled>
+          <HeaderMenuItem>
+            <Link
+              className="header__navigation__link header__navigation__link--blog"
+              to="/"
+            >
+              Blog
+            </Link>
+          </HeaderMenuItem>
+          <HeaderMenuItem>
+            <Link
+              className="header__navigation__link header__navigation__link--about"
+              to="/about"
+            >
+              About me
+            </Link>
+          </HeaderMenuItem>
+        </HeaderMenu>
+      </HeaderNavigation>
+    </StyledHeader>
   )
 }
 
