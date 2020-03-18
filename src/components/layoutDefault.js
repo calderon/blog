813bf516db0a7d7c1a89c109ccb4d890c8750a79
Daniel Calderon <daniel.calderon.portillo@gmail.com>
2020-03-18 18:13:38 +0100

    #3 Update default layout

--- src/components/layoutDefault.js
+++ src/components/layoutDefault.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { styled, ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import "normalize.css"
 
 import theme from "../theme"
@@ -7,7 +7,7 @@ import theme from "../theme"
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-    * {
+  * {
     &,
     &::before,
     &::after {
@@ -22,17 +22,61 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: ${props => props.theme.font.base};
+    letter-spacing: 0.12em;
+    line-height: 1.5;
+    word-spacing: 0.16em;
+    background-color: ${props => props.theme.colors.light};
+  }
+
+  p {
+    margin: 0 0 1rem 0;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
 `
 
 import Header from "../components/Header"
+import Heading from "../components/Heading"
+import { Container } from "./Container"
+
+const Page = styled.div.attrs(props => ({
+  className: "page",
+}))`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  max-width: 1120px;
+  margin: 0 auto;
+`
+
+const Main = styled.main.attrs(props => ({
+  className: "main",
+}))`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  display: grid;
+`
 
 const LayoutDefault = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
-      <main>{children}</main>
+      <Page>
+        <Header />
+        <Container as="aside" bright>
+          <Heading srOnly>Welcome message</Heading>
+          <p>
+            Hi. I’m a <mark>front-end developer</mark> from Sevilla and this is
+            my personal site.
+          </p>
+
+          <p>
+            <mark>Be welcome.</mark>
+          </p>
+        </Container>
+        <Main>{children}</Main>
+      </Page>
     </ThemeProvider>
   )
 }
